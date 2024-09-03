c2a19fb3827045fc4e3367b7e7b75a5c371adbb8
ocurus <victor.galindo@escola.pr.gov.br>
2024-09-03 09:42:27 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -131,8 +131,8 @@ let raquetada;
 let ponto;
 let trilha;
 
-function preload() {
-  trilha = loadSound("sounds/trilha.mp3");
-  ponto = loadSound("sounds/ponto.mp3");
-  raquetada = loadSound("sounds/raquetada.mp3");
+//function preload() {
+ // trilha = loadSound("sounds/trilha.mp3");
+//  ponto = loadSound("sounds/ponto.mp3");
+  //raquetada = loadSound("sounds/raquetada.mp3");
 }
